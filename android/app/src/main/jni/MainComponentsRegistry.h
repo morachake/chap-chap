cbaace27b7dffa15ebda9220ce752ecfe5c8eb2e
jacob-d242 <jacobmoracha@gmail.com>
2023-01-20 17:14:55 +0300

    Updating project files

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/chapchap/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/chap_chap/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
