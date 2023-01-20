557b4c4aedfa6341da4e0b7f702bfb00c72bd800
jacob-d242 <jacobmoracha@gmail.com>
2023-01-20 18:21:46 +0300

    updating ined and home js,setting up liners

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+export const COLORS = {
+  primary: '#00AC4F',
+  lightPrimary: '#EAFFFF',
+};
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import {View, Text} from 'react-native';
 
 export default function HomeScreen() {
-    return (
-        <View>
-      
-  </View>)
-    
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
 }
