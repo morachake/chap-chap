4f6788b23394c72abf282f30608d2300340d99f5
jacob-d242 <jacobmoracha@gmail.com>
2023-01-22 00:28:16 +0300

    updating Home Screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import {View, SafeAreaView, StyleSheet, Image} from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  StyleSheet,
+  ImageBackground,
+  Text,
+} from 'react-native';
 import {IMAGES} from '../constants/index';
 import Btn from '../components/Btn';
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <Image source={IMAGES.Cleaner} style={styles.banner} />
-      <Btn title="Register" />
+      <ImageBackground source={IMAGES.Cleaner} style={styles.banner} />
+      <View>
+        <Text style={styles.textAd}>Best Home Services for You</Text>
+        <Btn title="Register" />
+      </View>
     </SafeAreaView>
   );
 }
@@ -18,5 +27,10 @@ const styles = StyleSheet.create({
     resizeMode: 'cover',
     width: '100%',
     height: '90%',
+  },
+  textAd: {
+    fontFamily: 'Inter-Bold',
+    fontSize: 32,
+    color: '#000000',
   },
 });
