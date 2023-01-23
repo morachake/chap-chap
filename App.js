f6a51f14d4504e81a707dfb2550d586cc3849dc4
jacob-d242 <jacobmoracha@gmail.com>
2023-01-24 00:42:41 +0300

    Login screen complete

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 import ResetPassword from './src/screens/ResetPassScreen';
+import LoginScreen from './src/screens/LoginScreen';
 
 export default function App() {
   return (
     <SafeAreaView>
-      <ResetPassword />
+      <LoginScreen />
     </SafeAreaView>
   );
 }
