515255ce409078d319606b2216106cb931f4e36f
jacob-d242 <jacobmoracha@gmail.com>
2023-01-24 00:21:44 +0300

    Completed email verification screen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 import RegisterScreen from './src/screens/RegisterScreen';
+import VerifyEmail from './src/screens/VerifyEmail';
 
 export default function App() {
   return (
     <SafeAreaView>
-      <RegisterScreen />
+      <VerifyEmail/>
     </SafeAreaView>
   );
 }
---
+++ src/screens/VerifyEmail.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Header from '../components/Header';
+import MainInput from '../components/MainInput';
+import Btn from '../components/Btn';
+export default function VerifyEmail() {
+  return (
+    <View style={{display: 'flex', backgroundColor: '#F5F5F5'}}>
+      <Header pagename="Home" iconLeft="arrowleft" iconright={undefined} />
+      <Text style={styles.heading}>Verify Your Email</Text>
+      <View style={{justifyContent: 'center', alignItems: 'center'}}>
+        <MainInput label="Verification code" />
+      </View>
+      <View style={styles.btncontainer}>
+        <Btn title="Register" />
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  heading: {
+    fontFamily: 'Poppins-Bold',
+    fontSize: 32,
+    color: '#000000',
+    margin: 20,
+  },
+  btncontainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '75%',
+    bottom: 0,
+  },
+});
