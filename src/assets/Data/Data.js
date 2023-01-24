adccf60e7dee9deb9d6a9432111ee667481d83fe
jacob-d242 <jacobmoracha@gmail.com>
2023-01-24 17:21:15 +0300

    Adding dummy data for services

---
+++ src/assets/Data/Data.js
@@ -0,0 +1,24 @@
+const servicesData = [
+  {
+    title: 'Cleaning',
+    index: 1,
+    image: require('../images/clean.png'),
+  },
+  {
+    title: 'Repairs',
+    index: 2,
+    image: require('../images/mechanic.png'),
+  },
+  {
+    title: 'Laundary',
+    index: 3,
+    image: require('../images/plumber.png'),
+  },
+  {
+    title: 'Gardening',
+    index: 3,
+    image: require('../images/spanner.png'),
+  },
+];
+
+export default servicesData;
