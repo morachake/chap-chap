98cf7a219f210c16df43ca94855ef2dd27bd6839
jacob-d242 <jacobmoracha@gmail.com>
2023-01-26 18:46:31 +0300

    completing user profile

---
+++ src/components/UserDetailsCard.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+export default function UserDetailsCard({iconame,headline,title}) {
+  return (
+    <View style={styles.detailscard}>
+                  <MaterialCommunityIcons name={iconame} size={30} color="#00748D" style={ styles.icons} />
+                  <View>
+              <Text style={{ fontFamily: 'Inter-Bold', ...styles.headlines }}>{ headline}</Text>
+              <Text style={{ fontFamily: 'Inter-Italic', ...styles.headlines }}>{title}</Text>
+                  </View>
+              </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    icons: {
+        marginHorizontal:20
+    },
+    detailscard: {
+        alignItems: 'center',
+        borderRadius:8,
+        flexDirection: 'row',
+        //justifyContent: 'space-around',
+        width: '90%',
+        backgroundColor: '#F5F5F5',
+        height: 65,
+        marginTop: 15
+    },
+    headlines: {
+        color: '#000000',
+        padding: 3,
+    }
+})
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,10 +1,54 @@
 import React from 'react'
-import { View } from 'react-native'
-
+import { View, Text, Image, StyleSheet } from 'react-native'
+import Header from '../components/Header'
+import {IMAGES} from '../constants/index'
+import EvilIcons from 'react-native-vector-icons/EvilIcons'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import UserDetailsCard from '../components/UserDetailsCard'
 export default function ProfileScreen() {
   return (
-      <View>
-          
+      <View style={{ flex: 1, backgroundColor: '#008AAD'}}>
+          <View style={{ flex: 1,justifyContent: 'center' }}>
+              <Header iconLeft="arrowleft" />
+              <View style={{alignItems:'center'}}>                
+                  <Text style={styles.username}>Munene Phineas</Text>
+                  <Image source={IMAGES.User} alt='User' style={{ width: 75, height: 75 }} />
+                  <View style={{flexDirection:'row',alignItems:'center',margin:5,backgroundColor:'#00748D',borderRadius:8}}>
+                      <Text style={{color:'#FFFF',marginHorizontal:15,fontSize:14}}>Edit</Text>
+                      <EvilIcons name='pencil' size={22} color='#FFFF' />
+                  </View>
+              </View>
+          </View>
+          <View style={{ flex: 1.8, backgroundColor: '#FFFFFF',alignItems:'center' }}>
+              <UserDetailsCard iconame="email" headline="Email Adress" title="phineas@gmail.com" />
+              <UserDetailsCard iconame="cellphone" headline="Phone" title="0720000000" />
+              <UserDetailsCard iconame="id-card" headline="ID Card" title="7858642" />
+              <UserDetailsCard iconame="lock-open-outline" headline="Reset password"  title="Reset Password"/>
+          </View>
    </View>
   )
 }
+
+const styles = StyleSheet.create({
+    username: {
+        fontFamily: 'Poppins-Bold',
+        fontSize: 32,        
+    },
+    icons: {
+        marginHorizontal:20
+    },
+    detailscard: {
+        alignItems: 'center',
+        borderRadius:8,
+        flexDirection: 'row',
+        //justifyContent: 'space-around',
+        width: '90%',
+        backgroundColor: '#F5F5F5',
+        height: 65,
+        marginTop: 15
+    },
+    headlines: {
+        color: '#000000',
+        padding: 3,
+    }
+})
