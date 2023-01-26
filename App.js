7439473470f219f0fca572dffde1218e2cea946b
jacob-d242 <jacobmoracha@gmail.com>
2023-01-26 17:22:20 +0300

    Creating a user profile screen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import CleanerScreen from './src/screens/CleanerScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
 
 export default function App() {
   return (
     <>
-      <CleanerScreen/>
+      <ProfileScreen/>
     </>
   );
 }
