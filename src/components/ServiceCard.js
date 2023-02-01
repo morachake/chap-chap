26a1f88c89654c893e58419cef3a09f0adc0aa3f
jacob-d242 <jacobmoracha@gmail.com>
2023-02-01 11:46:41 +0300

    Updting service card

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Image, ScrollView, Text, View, StyleSheet, TouchableOpacity} from 'react-native'
 import { IMAGES } from '../constants/index'
 import { useNavigation } from '@react-navigation/native'
+import { ScaledSheet, scale } from 'react-native-size-matters'
 export default function ServiceCard() {
     const navigation= useNavigation()
     return (
         <TouchableOpacity onPress={() => navigation.navigate('Cleaner')}>
             <View style={styles.container}>
-                <View style={{borderRadius: 20}}>
-                    <Image source={IMAGES.Cleaner} resizeMode='cover' style={{resizeMode :'cover', width: 150, height: '100%'}} />
+                <View style={{borderRadius: scale(20)}}>
+                    <Image source={IMAGES.Cleaner} resizeMode='cover' style={{resizeMode :'cover', width: scale(150), height: '100%'}} />
                 </View>
                 <View style={{justifyContent:'center',marginLeft :15}}>
                     <Text style={styles.titles1}>10 Services Avalilable</Text>
@@ -19,21 +20,21 @@ export default function ServiceCard() {
     )
 }
 
-const styles = StyleSheet.create({
+const styles = ScaledSheet.create({
     container: {
-        height:90,
+        height:'90@s',
         flexDirection: 'row',
         backgroundColor: '#F5F5F5',
-        marginHorizontal: 15,
-        borderRadius:20
+        marginHorizontal: '15@s',
+        borderRadius:'20@s'
     },
     titles1: {
-        fontSize: 14,
+        fontSize: '14@s',
         fontFamily: 'Poppins-Bold',
-        padding:5
+        padding:'5@s'
     },
     titles: {
-        fontSize: 14,
+        fontSize: '14@s',
         fontFamily: 'Poppins-Light',
         
     }
