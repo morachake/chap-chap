b12fd7ee62439a97de9f8005bbb1911002d6d66c
jacob-d242 <jacobmoracha@gmail.com>
2023-02-01 11:41:42 +0300

    responsiveness for jobcard

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { scale } from 'react-native-size-matters';
 
 export default function JobCard({job,cleaner,date}) {
     return (
         <View style={{ flexDirection: 'row', alignItems: 'center', borderBottomWidth: 1, borderBottomColor: '#111', padding: 5,marginTop:7 }}>
             <View style={{ flex: 1 }}>
-                <Text style={{ fontSize: 18, fontFamily: 'Inter-Medium' }}>{job }</Text>
-                <Text style={{ fontSize: 14, fontFamily: 'Inter-Light' }}>{cleaner}</Text>
+                <Text style={{ fontSize: scale(18), fontFamily: 'Inter-Medium' }}>{job }</Text>
+                <Text style={{ fontSize: scale(14), fontFamily: 'Inter-Light' }}>{cleaner}</Text>
             </View>
-            <Text style={{ fontSize: 14, marginLeft: 20, fontFamily: 'Inter-Italic' }}>{date}</Text>
+            <Text style={{ fontSize: scale(14), marginLeft: scale(20), fontFamily: 'Inter-Italic' }}>{date}</Text>
         </View>
     );
 }
