046e58e13cd27818b213c94df921c5a6db0fb1e1
jacob-d242 <jacobmoracha@gmail.com>
2023-02-01 11:44:16 +0300

    using scalesheet to update reposnivaness

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { View, TextInput } from 'react-native';
+import { scale } from 'react-native-size-matters';
 import Icon from 'react-native-vector-icons/Feather';
 
 export default function SearchInput() {
@@ -8,7 +9,7 @@ export default function SearchInput() {
         <View style={{ width: '100%', backgroundColor: '#FFFF' }}>
             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                 <TextInput
-                    style={{ flex: 1, borderRadius: 25, color: '#0000' }}
+                    style={{ flex: 1, borderRadius: scale(25), color: '#0000' }}
                     placeholder="What are you looking for"
                     value={searchText}
                     onChangeText={text => setSearchText(text)}
@@ -16,7 +17,7 @@ export default function SearchInput() {
                 <Icon
                     name="search"
                     size={20}
-                    style={{ marginRight: 10 }}
+                    style={{ marginRight: scale(10) }}
                     onPress={() => console.log('Searching: ', searchText)}
                 />
             </View>
